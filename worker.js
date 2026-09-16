3885208226f4f4665407ec900b30839949e5c03d
kofiagyei79 <113058474+kofiagyei79@users.noreply.github.com>
2026-09-16 13:45:06 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,8 @@
-
+export default {
+  async fetch(request, env, ctx) {
+    const htmlBody = `
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
